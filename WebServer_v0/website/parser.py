e0c269a96f76130ddcfc9effab45c99d2a620da5
luvshah11 <lshah@depaul.edu>
2022-11-10 17:33:21 -0600

    Revert "little code clean up and added drivers directly into a folder"
    This reverts commit e01da59f64bbb85245ac6b6253182d5c73c4d8e4.

--- WebServer_v0/website/parser.py
+++ WebServer_v0/website/parser.py
@@ -14,14 +14,15 @@ URL = []
 
 
 def AmazonParser(path):
+    # results_path = 'WebServer_v0/website/csv/results.csv'
 
-    df = pd.read_csv(path)
     # Get current working directory
     #cwd = os.getcwd()
     #files = os.listdir(cwd)
     #print(f"Files in {cwd}: {files}")
 
     # creates a results.csv that is cleaner and does not contain headers. It is ready to scrape.
+    df = pd.read_csv(path)
     # df.to_csv(results_path, sep='\t', header=None, mode='a')
 
     # List of details to compare to
@@ -95,7 +96,14 @@ def getPrice():
 def getURL():
     df = pd.read_csv(path)
     for len_count, link in enumerate(df['Url']):
-        URL.append(link)
+        temp_URL = []
+        temp_URL.append(link)
+
+        try:
+            URL.append(temp_URL[0])
+        except:
+            URL.append('')
+
     # print(URL)
     # print(len(URL))
 
@@ -113,7 +121,6 @@ def createAmazonTuple():
 
     # print(records)
     return records
-
 
 # Use this to run the parser.py locally to test that it prints a tuple
 # createAmazonTuple()
